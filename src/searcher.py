463c58e5659d0a31bb0e1d5e62628c5dea556425
Arnaud OU <ou.light.arnaud@gmail.com>
2016-12-27 07:10:39 +0100

    misc: add comment and README

--- src/searcher.py
+++ src/searcher.py
@@ -4,6 +4,8 @@ import data
 def count(date):
     count = 0
     for key, values in data.data.items():
+        # queries are sorted by their first date and
+        # their dates are themselves sorted
         if values[0][:len(date)] > date:
             break
         for value in values:
@@ -13,12 +15,13 @@ def count(date):
     return json_count(count)
 
 def json_count(count):
-    return "{ count: " + str(count) + " }"
+    return "{ count: " + str(count) + " }\n"
 
 
 def popular(date, size):
     if size == 0:
         return None
+    # Popular is used as a heap
     popular = []
     for key, values in data.data.items():
         count = 0
@@ -33,8 +36,13 @@ def popular(date, size):
             continue
         if len(popular) < size:
             heapq.heappush(popular, (count, key))
+        # nsmallest returns a list of the n smallest
+        # [0] is for the first element of the list
+        # [0][0] is for the count of this element
         elif count > heapq.nsmallest(1, popular)[0][0]:
             heapq.heapreplace(popular, (count, key))
+    # Sort the list by their count from the smaller to the bigger
+    # [::-1] reverse the list
     popular = sorted(popular, key=lambda tup: tup[0])[::-1]
     return json_pop(popular)
 
@@ -43,7 +51,7 @@ def json_pop(heap):
     for values in heap:
         json += "{ query: \"" + values[1] + "\", count: " + str(values[0]) + " },\n    "
     json = json[:-2]
-    json += "]\n}"
+    json += "]\n}\n"
     print(json)
     return json
 
